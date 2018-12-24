7aeff2203b87d681fe7e185fa802dec07f0c1e79
Anden Acitelli <a.acitelli@gmail.com>
2018-12-23 19:00:55 -0500

    Implemented searching for a specific pokemon

--- pageBehavior.js
+++ pageBehavior.js
@@ -4,12 +4,55 @@ document.addEventListener("DOMContentLoaded", function()
     getRandomPokemon();
 });
 
+// Todo - Make this a single function with "modes" or smth like that 
+// Same as the randomized one but uses 
+function getPokemon(inputStr)
+{
+    // Getting the pokemon data 
+    fetch("https://pokeapi.co/api/v2/pokemon/" + inputStr + "/")    
+    .then(function(response)
+    {
+        if (!response.ok)
+        {
+            console.log("Pokemon Response: Fail. Throwing error.");
+            throw new Error("HTTP Error " + response.status);
+        }
+
+        return response.json();
+    })    
+    .then(function(pokemonData)
+    {        
+        fetch(pokemonData.species.url)
+        .then(function(response)
+        {
+            if (!response.ok)
+            {
+                console.log("Species Response: Fail. Throwing error.");
+            }
+
+            return response.json();
+        })
+        .then(function(speciesData)
+        {
+            // Both of these objects are full of data, so can be passed
+            updatePage(pokemonData, speciesData);
+        })
+        .catch(function(err)
+        {
+            console.log("Failed to fetch species data. Error: " + err);
+        });
+    })
+    .catch(function(err)
+    {
+        console.log("Failed to fetch pokemon data. Error: " + err);
+    });
+}
+
 function getRandomPokemon()
 {
     // Total # of pokemon in database
     let pokemonNum = Math.floor(Math.random() * 802 + 1);
 
-    console.log("JSON File 1 URL: " + "https://pokeapi.co/api/v2/pokemon/" + pokemonNum + "/")
     // Getting the pokemon data 
     fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonNum + "/")    
     .then(function(response)
@@ -103,4 +146,17 @@ function setFlavorText(pokemonData, speciesData)
 document.getElementById("randomizeButton").addEventListener("click", function()
 {
     getRandomPokemon();
+});
+
+// Fired when ANY key is pressed  
+document.getElementById("textInput").addEventListener("keypress", function (e) 
+{
+    console.log("Event listener fired");
+    var key = e.which || e.keyCode;
+
+    if (key === 13)
+    { 
+      let pokemonName = document.getElementById("textInput").value;
+      getPokemon(pokemonName.toLowerCase());
+    }
 });
